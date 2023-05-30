9a7ef2ed459491dbacc23ebbcbf9d086da840610
kenny1184 <37927922+kenny1184@users.noreply.github.com>
2023-05-30 17:19:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+string CurrFolder = Environment.CurrentDirectory;
+
+var folder = CurrFolder;
+int count = 0;
+
+foreach (string file in Directory.GetFiles(folder))
+{
+    string ext = Path.GetExtension(file).ToLower();
+    if (ext == ".png")
+    {
+        Console.WriteLine(file);
+        string name = Path.GetFileNameWithoutExtension(file);
+
+        Image png = Image.FromFile(file);
+
+        png.Save(CurrFolder + @"/" + name + ".jpg", ImageFormat.Jpeg);
+        png.Dispose();
+
+        count++;
+        File.Delete(file);
+
+    }
+}
+
+Console.WriteLine("{0} file(s) converted.", count);
+Console.ReadLine();
